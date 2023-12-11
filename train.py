8cbf9b1695e4d2b305a620b851b654ef8b42c4f0
Quang Hien <130220114+QuagHien@users.noreply.github.com>
2023-12-12 02:00:32 +0700

    Update train.py

--- train.py
+++ train.py
@@ -56,8 +56,8 @@ def compute_metrics(p):
 training_args = TrainingArguments(
     warmup_ratio=0.1,
     learning_rate=2e-5,
-    per_device_train_batch_size=2,
-    per_device_eval_batch_size=2,
+    per_device_train_batch_size=1,
+    per_device_eval_batch_size=1,
     num_train_epochs=15,
     report_to='none',
     output_dir = f'./checkpoints_{VER}',
