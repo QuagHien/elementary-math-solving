549ea790196cf3fb8f569bc55187c3e0a08642d9
Quang Hien <130220114+QuagHien@users.noreply.github.com>
2023-12-12 01:04:30 +0700

    Update train.py

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from dataloader import VER, NUM_TRAIN_SAMPLES, USE_PEFT, FREEZE_LAYERS, FREEZE_E
 
 
 ######
-model = AutoModelForMultipleChoice.from_pretrained('MODEL')
+model = AutoModelForMultipleChoice.from_pretrained('microsoft/deberta-v3-large')
 
 ######
 if USE_PEFT:
